7d65e9d2e6432d690a24b2e1dfce1f4c6826e0d6
daniellemdanis <143761200+daniellemdanis@users.noreply.github.com>
2023-12-21 23:24:06 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,40 +13,102 @@ let earthquake_markers = [];
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
 // Get the data with d3.
-d3.json(url).then(function(response) {
+d3.json(url).then(function (response) {
 
     features = response.features;
 
     function getColor(depth) {
-        if (depth <=  5) {
-            return .25;
-        }
-        else if (depth <= 10) {
-            return .5;
-        }
-        else {
-            return .75
+        if (depth <= 5) {
+            return 'green';
+        } else if (depth <= 10) {
+            return 'yellow';
+        } else {
+            return 'red';
         }
     }
+
+    let legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 5, 10],
+            labels = [];
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    let info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
+    };
+
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>Earthquake Info</h4>' + (props ?
+            '<b>Magnitude:</b>' + props.magnitude.toFixed(2) + '<br />' + '<b>Depth:</b>' + props.depth.toFixed(2) + ' km'
+            : 'Hover over an earthquake');
+    };
+
+    function highlightFeature(e) {
+        let layer = e.target;
+
+        layer.setStyle({
+            weight: 2,
+            dashArray: '',
+            fillOpacity: 0.75
+        });
+
+        layer.bringToFront();
+
+        let props = {
+            magnitude: layer.options.radius / 15000,
+            depth: layer.getLatLng().lat
+        };
+
+        info.update(props);
+    }
+
+    function resetHighlight(e) {
+        let layer = e.target;
+
+        layer.setStyle({
+            weight: 0.5,
+            fillOpacity: .75
+        });
+
+        info.update();
+    }
+
+    legend.addTo(myMap);
+    info.addTo(myMap);
 
     for (let i = 0; i < features.length; i++) {
         let location = features[i].geometry;
         let magnitude = features[i].properties;
         let depth = location.coordinates[2];
 
-        earthquake_markers.push(
-            L.circle([location.coordinates[1], location.coordinates[0]], {
-                stroke: false,
-                fillOpacity: getColor(depth),
-                color: 'red',
-                radius: (magnitude.mag)*10000
-            })
-        );
+        let circleMarker = L.circle([location.coordinates[1], location.coordinates[0]], {
+            stroke: false,
+            fillOpacity: .75,
+            color: getColor(depth),
+            radius: (magnitude.mag) * 15000
+        });
+
+        circleMarker.on('mouseover', highlightFeature);
+        circleMarker.on('mouseout', resetHighlight);
+
+        earthquake_markers.push(circleMarker);
     }
 
     let quakes = L.layerGroup(earthquake_markers);
 
     // Add the earthquake markers layer to the map
     quakes.addTo(myMap);
-
 });
